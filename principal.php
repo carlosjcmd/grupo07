2a949bc340d60b535ef111d0fff9af1bfd011725
carlosjcmd <carlosjcmd@gmail.com>
2022-06-12 22:01:13 -0500

    comentario

---
+++ principal.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <div class="">
+      <p>
+      </p>
+    </div>
+  </body>
+</html>
+
+<?php
+  for ($i=0; $i < ; $i++) {
+    // code...
+  }
+  if (a>10) {
+    // code...
+  }
+  while ($a <= 10) {
+    // code...
+  }
+  
+ ?>
+
+ <?php
+
+  ?>
